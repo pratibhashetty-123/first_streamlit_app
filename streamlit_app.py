fa009680ff20f6abf23d87346e0bf78e7da9cff1
pratibhashetty-123 <53473162+pratibhashetty-123@users.noreply.github.com>
2023-06-08 13:29:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('healthy dinner')
-streamlit.header('breakpast menu')
-streamlit.text('idly')
-streamlit.text('dosa')
-streamlit.text('uppittu')
-streamlit.text('chapathi')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
