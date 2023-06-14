dc72033b3a8449427c64747bf3f06b53b88a4c1e
pratibhashetty-123 <53473162+pratibhashetty-123@users.noreply.github.com>
2023-06-14 10:48:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,4 +68,6 @@ streamlit.dataframe(my_data_rows)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding Jackfruit ', fruit_choice)
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
+
 
