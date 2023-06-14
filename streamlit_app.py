efb794bf7110192eb2b4e4dd3286cae0409e8e75
pratibhashetty-123 <53473162+pratibhashetty-123@users.noreply.github.com>
2023-06-14 16:56:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,34 +4,18 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 streamlit.title("My Mom's New Healthy Diner")
-
- 
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
- 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
- 
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
- 
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
- 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
@@ -52,17 +36,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-# streamlit.text(fruityvice_response.json()) # just writes the data to the screen - removing
 
- 
-
-# take the json version of the response and normalize it
-
-
-
- 
-
-# output it to the screen as table
 
 #streamlit.stop()
 def get_fruit_load_list():
